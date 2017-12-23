da52e5252e607ca2f451518d9fb4568e54de419c
Jeff Niu <jeffniu22@gmail.com>
2017-12-23 15:34:07 -0800

    Modified crc_table_init

--- assembly-crc/crc_table_init.c
+++ assembly-crc/crc_table_init.c
@@ -4,7 +4,7 @@ void crc_table_init(uint8_t table[256]) {
     uint8_t remainder;
     for (uint8_t dividend = 0; ; ++dividend) {
         remainder = dividend;
-        for (uint8_t bit = 8; bit > 0; --bit) {
+        for (uint8_t bit = 0; bit < 8; ++bit) {
             if (remainder & 0x80) {
                 remainder = (remainder << 1) ^ DIVISOR;
             } else {
